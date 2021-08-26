9f90d2e6e36d4c7c67e93ae3a71989a56ce1448c
NikkiA5 <35671561+NikkiA5@users.noreply.github.com>
2021-08-26 19:35:25 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='YOLO-digit-detector',
+      version=0,
+      description='number detector',
+      author='Nikki A',
+      author_email='abuschik.nika@mail.ru',
+      url='nikkia5.github.io',
+      packages=['yolo3', 'model_data', 'font'],  
+     )
